64cf2625d9810ec678cdda5f94a9b095583935b0
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2024-08-09 11:46:51 -0500

    Ajustes interfaz

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -2,11 +2,19 @@ import 'react-native-gesture-handler';
 
 import {StackNavigator} from './presentation/navigator/StackNavigator';
 import {NavigationContainer} from '@react-navigation/native';
+import {PaperProvider} from 'react-native-paper';
+import {ThemeContextProvider} from './presentation/context/ThemeContext';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient();
 
 export const PokedexApp = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContextProvider>
+        <StackNavigator />
+      </ThemeContextProvider>
+    </QueryClientProvider>
   );
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,68 @@
-import {Text, View} from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
+import {Text} from 'react-native-paper';
+import {getPokemons} from '../../../actions/pokemons';
+import {
+  useInfiniteQuery,
+  useQueryClient,
+  QueryClient,
+} from '@tanstack/react-query';
+import {PokeballBg} from '../../components/ui/PokeballBg';
+import {Pokemon} from '../../../domain/entities/pokemon';
+import {globalTheme} from '../../../config/theme/theme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {PokemonCard} from '../../components/pokemons/PokemonCard';
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
+
+  const queryClient = useQueryClient();
+
+  /*
+  // Forma tradicional de una petición http
+  const {isLoading, data: pokemons = []} = useQuery({
+    queryKey: ['pokemons'],
+    queryFn: () => getPokemons(0, 20),
+    staleTime: 1000 * 60 * 60, // 60 minutos
+  });
+  */
+
+  const {isLoading, data, fetchNextPage} = useInfiniteQuery({
+    queryKey: ['pokemons', 'infinite'],
+    initialPageParam: 0,
+    staleTime: 1000 * 60 * 60, // 60 minutos
+    queryFn: async params => {
+      const pokemons = await getPokemons(params.pageParam);
+
+      pokemons.forEach(pokemon => {
+        queryClient.setQueryData(['pokemons', pokemon.id], pokemon);
+      });
+
+      return pokemons;
+    },
+    getNextPageParam: (lastPage, pages) => pages.length,
+  });
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={globalTheme.globalMargin}>
+      <PokeballBg style={styles.imgPosition} />
+      <FlatList
+        data={data?.pages.flat() ?? []}
+        keyExtractor={(pokemon: Pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
+        style={{paddingTop: top + 20}}
+        ListHeaderComponent={() => <Text variant="displayMedium">Pokedex</Text>}
+        renderItem={({item: pokemon}) => <PokemonCard pokemon={pokemon} />}
+        onEndReachedThreshold={0.6}
+        onEndReached={() => fetchNextPage()}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imgPosition: {
+    position: 'absolute',
+    top: -100,
+  },
+});
